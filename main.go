4fd06fa179b850787b6bb1c9b4a5107351ba6248
JAE HO LEE <spectrick@gmail.com>
2017-02-01 22:39:51 +0900

    rules 파일 적용 command line 에서 field delimiter를 \t 으로 구분한 text file을 두번째 인자로 지정 첫번째 필드는 체크하고자 하는 정규식 두번째 필드는 replace에서 교체하고자 하는 문자열로 구성한다.

--- main.go
+++ main.go
@@ -4,26 +4,40 @@ import (
 	"os"
 	"io/ioutil"
 	"regexp"
+	"bufio"
+	"strings"
 )
 
 func main() {
+	if len(os.Args)<3 {
+		showUsage()
+		return
+	}
 	src := getSource(os.Args[1])
 	str := src
-	str = regReplacer(str, `(?im)<script[^>]*>[\s\S]*?</script>`, "")
-	str = regReplacer(str, `(?im)<noscript[^>]*>[\s\S]*?</noscript>`, "")
-	str = regReplacer(str, `(?im)<!--[\s\S]*?-->`, "")
-	str = regReplacer(str, `(?im)<style[^>]*>[\s\S]*?</style>`, "")
-	str = regReplacer(str, `(?im)<html[^>]*>`, "")
-	str = regReplacer(str, `(?im)<head>[\s\S]*?</head>`, "")
-	str = regReplacer(str, `(?im)<article[^>]*>([\s\S]*?)</article>`, "$1")
-	str = regReplacer(str, `(?im)(<(head|iframe|body|section|meta|title)[^>]*>)`, "\n$1")
-	str = regReplacer(str, `(?im)(<(div|li|ul|h1|h2|h3|h4|p|img)[^>]*>)`, "\n  $1")
-	str = regReplacer(str, `(?im)(<section[^>]*>)`, "\n$1")
-	str = regReplacer(str, `(?im)<li value="[\d]*" [^>]*>`, `<li class="list">`)
-	str = regReplacer(str, `(?im)<section class="slide-content" [^>]*>`, `<section class="slides">`)
-	/* font wegiht */
-	str = regReplacer(str, `(?im)<p style="font-weight:([\d]*)[^>]*>`, `<p class="fw$1">`)
+	file, err := os.Open(os.Args[2])
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line[0] != 35 {
+			tokens := strings.Split(line, "\t")
+			if len(tokens)>1 {
+				/* file 에서 읽은 \n 을 newline으로 대체 */
+				str = regReplacer(str, tokens[0], strings.Replace(tokens[1], "\\n", "\n", -1 ))
+			} else {
+				str = regReplacer(str, tokens[0], "")
+			}
+		}
+	}
 	println(str)
+}
+func showUsage() {
+	println(`
+Usage: regexnogada <source file> <rule file>
+	`)
 }
 func regReplacer(source string, regex string, replacement string) string {
 	return regexp.MustCompile(regex).ReplaceAllString(source, replacement)
